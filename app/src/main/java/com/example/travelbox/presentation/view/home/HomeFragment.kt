186cea6125698142068e9982fe5750228b82468d
JAY <wndkdus102@naver.com>
2025-01-21 17:57:01 +0900

    [Feat] 홈 배너, 게시물 추가

--- app/src/main/java/com/example/travelbox/presentation/view/home/HomeFragment.kt
+++ app/src/main/java/com/example/travelbox/presentation/view/home/HomeFragment.kt
@@ -1,10 +1,20 @@
 package com.example.travelbox.presentation.view.home
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.text.style.RelativeSizeSpan
+import android.text.style.StyleSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.example.travelbox.R
 import com.example.travelbox.databinding.FragmentHomeBinding
 
@@ -22,9 +32,16 @@ class HomeFragment : Fragment() {
 
     lateinit var binding : FragmentHomeBinding
 
+    private lateinit var bannerAdapter: BannerAdapter
+    private lateinit var mBanner: ViewPager2
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
 
     }
 
@@ -34,27 +51,118 @@ class HomeFragment : Fragment() {
     ): View? {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+
+        setTextStyle()
+
+
+
+
+        mBanner = binding.bannerList
+        bannerAdapter = BannerAdapter(this, 3)
+        mBanner.adapter = bannerAdapter
+
+        //수평 슬라이드 설정
+        mBanner.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        // 배너 시작지점 설정
+        mBanner.setCurrentItem(1)
+
+
+        // 페이지 한 개씩 미리 로드
+        mBanner.offscreenPageLimit = 3
+
+
+
+
+        // 페이지 전환 애니메이션 및 캐러셀 효과
+        val pageMargin = resources.getDimensionPixelOffset(R.dimen.pageMargin)
+        val pageOffset = resources.getDimensionPixelOffset(R.dimen.offset)
+
+        mBanner.setPageTransformer { page, position ->
+            val myOffset = position * -(2 * pageOffset + pageMargin)
+            if (position < -1) {
+                page.setTranslationX(-myOffset)
+            } else if (position <= 1) {
+                val scaleFactor = Math.max(0.7f, 1 - Math.abs(position - 0.14285715f))
+                page.setTranslationX(myOffset)
+                page.setScaleY(scaleFactor)
+                page.setAlpha(scaleFactor)
+            } else {
+                page.setAlpha(0f)
+                page.setTranslationX(myOffset)
+            }
+
+
+        }
+
+        // 배너 양 옆에 배너가 보이도록 패딩 설정
+        mBanner.setPadding(pageMargin, 0, pageMargin, 0)
+        mBanner.clipToPadding = false
+
+
+        mBanner.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                // 페이지가 스크롤 될 때 이벤트 처리
+                if (positionOffsetPixels == 0) {
+                    mBanner.setCurrentItem(position)
+                }
+            }
+
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+            }
+        })
+
+
+
+
+
+
+
+
         return binding.root
 
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    private fun setTextStyle() {
+        val content = binding.tvTravelRecord.text.toString()
+        val spannableString = SpannableString(content)
+
+        val start = content.indexOf("여행 기록")
+        val end = start + "여행 기록".length
+
+        if(start >= 0) {
+            spannableString.setSpan(
+                ForegroundColorSpan(Color.parseColor("#007151")),
+                start,
+                end,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+
+            spannableString.setSpan(
+                StyleSpan(Typeface.BOLD),
+                start,
+                end,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+
+            spannableString.setSpan(
+                RelativeSizeSpan(1.3f),
+                start,
+                end,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
+
+
+        binding.tvTravelRecord.text = spannableString
     }
+
+
 }
