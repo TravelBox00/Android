ddb6adcacf26e1031b0c697eadf87ef4bc26f0e4
JAY <wndkdus102@naver.com>
2025-02-04 15:17:34 +0900

    [feat] 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,21 @@ dependencies {
     implementation ("com.jakewharton.threetenabp:threetenabp:1.4.4")
 
 
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+    implementation("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
+
+    // okHttp
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+
+
+
 
 
 }
