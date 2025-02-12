97efba90ac91c86683939915470bd85a7a4a7b68
JAY <wndkdus102@naver.com>
2025-02-12 19:51:21 +0900

    [feat] 배너 텍스트 추가

--- app/src/main/java/com/example/travelbox/presentation/view/home/Banner2Fragment.kt
+++ app/src/main/java/com/example/travelbox/presentation/view/home/Banner2Fragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.travelbox.R
+import com.example.travelbox.databinding.FragmentBanner1Binding
+import com.example.travelbox.databinding.FragmentBanner2Binding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,24 +20,31 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Banner2Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var binding: FragmentBanner2Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_banner2, container, false)
+        binding = FragmentBanner2Binding.inflate(inflater, container, false)
+
+        // 배너 텍스트
+
+        val tvTitle = binding.tvTitle
+
+        val tvDescription = binding.tvDescription
+
+
+        tvTitle.text = "교토"
+        tvDescription.text = "고요한 전통과 현대의 조화"
+
+
+        return binding.root
     }
 
     companion object {
