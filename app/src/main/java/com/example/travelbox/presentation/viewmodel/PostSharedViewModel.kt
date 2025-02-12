b024a7cc0f1f4b1e57fb49261d5de26eedf6f31c
JAY <wndkdus102@naver.com>
2025-02-12 19:52:34 +0900

    [add] 인기 게시물 이미지 viewModel 생성

---
+++ app/src/main/java/com/example/travelbox/presentation/viewmodel/PostSharedViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.travelbox.presentation.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+
+
+// 인기 게시물 이미지
+class PostSharedViewModel : ViewModel(){
+    private val _topImages = MutableLiveData<List<String>>()
+    val topImages: LiveData<List<String>> get() = _topImages
+
+    fun setTopImages(images: List<String>) {
+        _topImages.value = images
+    }
+}
