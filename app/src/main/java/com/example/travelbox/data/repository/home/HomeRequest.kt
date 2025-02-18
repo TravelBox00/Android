cfebbbbeba97ef83c61d339e9b90d77de09f215b
JAY <wndkdus102@naver.com>
2025-02-18 21:55:01 +0900

    [feat] 좋아요, 북마크 api 연결

--- app/src/main/java/com/example/travelbox/data/repository/home/HomeRequest.kt
+++ app/src/main/java/com/example/travelbox/data/repository/home/HomeRequest.kt
@@ -2,3 +2,25 @@ package com.example.travelbox.data.repository.home
 
 
 // 댓글 추가
+data class CommentRequest(
+    val userTag: String,
+    val threadId: Int,
+    val commentContent: String,
+    val commentVisible: String
+)
+
+
+// 게시글 좋아요 상태
+data class PostLikeRequest(
+
+    val threadId: Int,
+    val userTag: String
+)
+
+
+// 게시글 북마크 상태
+data class PostScrapRequest(
+
+    val threadId: Int,
+    val userTag: String
+)
